c8fbcef6580f5e83737d1e803529703916ec0c49
PetrenkoEkaterina <shefbest@gmail.com>
2023-10-30 23:05:46 +0300

    New

--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -1,6 +1,6 @@
 public class Square {
     public static void main(String[] args) {
-        int side = 6;
+        int side = 7;
         int area = side * side;
 
         System.out.println("Площадь квадрата " + area);
