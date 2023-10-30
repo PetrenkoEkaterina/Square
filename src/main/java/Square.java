59e241eaf39c7148740246b012ec991bcfc0bc6f
PetrenkoEkaterina <shefbest@gmail.com>
2023-10-30 23:03:56 +0300

    New

---
+++ src/main/java/Square.java
@@ -0,0 +1,8 @@
+public class Square {
+    public static void main(String[] args) {
+        int side = 6;
+        int area = side * side;
+
+        System.out.println("Площадь квадрата " + area);
+    }
+}
